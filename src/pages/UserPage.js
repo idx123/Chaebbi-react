8be09311dc9bf2355c2e0b0467d7105524033e0f
idx123 <jinhalee020@gmail.com>
2023-02-23 01:42:35 +0900

    [Feat] 유저 개인 페이지 생성
    마이페이지

---
+++ src/pages/UserPage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const UserPage =()=>{
+    return(
+        <div>
+            <h1>유저개인페이지</h1>
+        </div>
+    )
+}
+
+export default UserPage;
