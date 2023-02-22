31de0b63d32da684a48f1195ad7d41a2a1854b69
idx123 <jinhalee020@gmail.com>
2023-02-23 01:41:25 +0900

    [Feat] 식단기록 페이지 생성

---
+++ src/pages/RecordSearch.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const RecordSearch =()=>{
+    return(
+        <div>
+            <h1>식단기록(메뉴검색)</h1>
+        </div>
+    )
+}
+
+export default RecordSearch;
