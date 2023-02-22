69ede0d793930395aac5146dbe63ef08ec6ecfad
idx123 <jinhalee020@gmail.com>
2023-02-23 01:41:56 +0900

    [Feat] 식단분석 페이지 생성

---
+++ src/pages/AnalyzeDiet.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const AnalyzeDiet =()=>{
+    return(
+        <div>
+            <h1>식사패턴분석페이지</h1>
+        </div>
+    )
+}
+
+export default AnalyzeDiet;
