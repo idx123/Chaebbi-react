ec50f98cf6221e1c1218692b43148961009a5505
idx123 <jinhalee020@gmail.com>
2023-02-23 01:42:09 +0900

    [Feat] 음식점 검색 페이지 생성

---
+++ src/pages/SearchRestaurant.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const SearchRestaurant =()=>{
+    return(
+        <div>
+            <h1>음식점검색페이지</h1>
+        </div>
+    )
+}
+
+export default SearchRestaurant;
