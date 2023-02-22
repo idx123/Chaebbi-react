5ce9c1cc8628c2fed2c23f11aa60e4c08d734ecd
idx123 <jinhalee020@gmail.com>
2023-02-23 01:40:07 +0900

    [Feat] 경로 지정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './utils/GlobalStyle';
+import Navbar from './components/Navbar';
+import RecordSearch from './pages/RecordSearch';
+import RecordImage from './pages/RecordImage';
+import RecommendFridge from './pages/RecommendFridge';
+import SearchRestaurant from './pages/SearchRestaurant';
+import AnalyzeDiet from './pages/AnalyzeDiet';
+import UserLogin from './pages/UserLogin';
+import UserRegister from './pages/UserRegister';
+import UserPage from './pages/UserPage';
+import Landing from './pages/Landing';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+    <BrowserRouter>
+      <Navbar>
+          <Routes>
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/record-by-image" element={<RecordImage/>}/>
+            <Route path="/record-by-keyword" element={<RecordSearch/>}/>
+            <Route path="/analyze-diet" element={<AnalyzeDiet/>}/>
+            <Route path="/recommend" element={<RecommendFridge/>}/>
+            <Route path="/search" element={<SearchRestaurant/>}/>
+            <Route path="/mypage" element={<UserPage/>}/>
+            <Route path="/sign-in" element={<UserLogin/>}/>
+            <Route path="/sign-up" element={<UserRegister/>}/>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>
+        </Navbar>
+    </BrowserRouter>
+    </>
   );
 }
 
