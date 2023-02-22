992657fd0d8a4e7571c074f15dc672df732b0ef1
idx123 <jinhalee020@gmail.com>
2023-02-23 01:41:43 +0900

    [Feat] 메뉴추천 페이지 생성

---
+++ src/pages/RecommendFridge.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const RecommendFridge =()=>{
+    return(
+        <div>
+            <h1>메뉴추천</h1>
+        </div>
+    )
+}
+
+export default RecommendFridge;
