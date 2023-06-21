5721e38a3a21d8a37431f67c4750f757b3793f52
idx123 <jinhalee020@gmail.com>
2023-06-21 17:33:28 +0900

    feat : 오류, 성공 메세지 추가

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,42 +1,69 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-const Input =(props)=>{
-    const {id, label, type, placeholder, _onChange, margin} = props;
-    return (
-        <Container margin={margin}>
-            <StyledLabel htmlFor={id}>{label}</StyledLabel>
-            <StyledInput id={id} type={type} placeholder={placeholder} onChange={_onChange}/>
-        </Container>
-    )
+const Input = (props) => {
+  const {
+    id,
+    label,
+    type,
+    placeholder,
+    _onChange,
+    errorMessage,
+    successMessage,
+  } = props
+  return (
+    <Container>
+      <StyledLabel htmlFor={id}>{label}</StyledLabel>
+      <StyledInput
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={_onChange}
+        autoComplete="off"
+      />
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      {successMessage && <p className="success">{successMessage}</p>}
+    </Container>
+  )
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.4rem;
-    font-size: 1.4rem;
-    margin: ${(props)=> props.margin ? props.margin : "0 0 1rem 0"};
-`;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  margin-bottom: 1rem;
+
+  p {
+    font-size: 1.2rem;
+  }
+
+  p.error {
+    color: var(--color-danger);
+  }
+  p.success {
+    color: var(--color-point);
+  }
+`
 
 const StyledLabel = styled.label`
-    position: relative;
-    left: 0.4rem;
-`;
+  position: relative;
+  left: 0.4rem;
+`
 
 const StyledInput = styled.input`
-    height: 4rem;
-    border: 1px solid #DCE5E7;
-    border-radius: 0.2rem;
-    padding: 0.5rem 1rem;
-    outline: none;
+  height: 4rem;
+  border: 1px solid #dce5e7;
+  border-radius: 0.2rem;
+  padding: 0.5rem 1rem;
+  outline: none;
 
-    &:focus, &:active{
-        border: 1.5px solid #19BC14;
-    }
-    
-    &::placeholder{
-        color: #B7C2C6; //서브컬러사용
-    }
-`;
+  &:focus,
+  &:active {
+    border: 1.5px solid #19bc14;
+  }
 
-export default Input;
+  &::placeholder {
+    color: var(--color-sub-text);
+  }
+`
+
+export default Input
